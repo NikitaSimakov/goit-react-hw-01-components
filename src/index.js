fa08c5e140d8e8e77fb1685832bbdcb72177db11
NikitaSimakov <mishaaniskin@gmail.com>
2023-04-28 08:31:49 +0300

    partipians

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import user from './data/user';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,45 +9,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 
-function Profile(props) {
-  const { avatar, username, tag, location, followers, views, likes } = props;
-  return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt="User avatar" class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">{tag}</p>
-        <p class="location">{location}</p>
-      </div>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{followers}</span>
-        </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{views}</span>
-        </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{likes}</span>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <Profile
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-  />,
-  document.querySelector('#root')
-);
-// console.log(root.render);
+// root.render(
+//   <Profile
+//     username={user.username}
+//     tag={user.tag}
+//     location={user.location}
+//     avatar={user.avatar}
+//     stats={user.stats}
+//   />,
+//   document.querySelector('#root')
+// );
