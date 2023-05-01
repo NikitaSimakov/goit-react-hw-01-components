6ca9958ee71d99793c68c49290ffdccf62f7f8b7
NikitaSimakov <mishaaniskin@gmail.com>
2023-05-01 20:35:39 +0300

    add random color

--- src/components/Transaction/TransactionItem.js
+++ src/components/Transaction/TransactionItem.js
@@ -1,9 +1,9 @@
+// import { clsx } from 'clsx';
 import css from './TransactionItem.module.css';
 export function TransactionItem({ transaction }) {
-  // const backgroundStyle = () => transaction.type === 'invoice' ?
   return (
     <tbody>
-      <tr className={`${transaction.type}`}>
+      <tr className={css[transaction.type]}>
         <td>{transaction.type}</td>
         <td>{transaction.amount}</td>
         <td>{transaction.currency}</td>
